aab93d0179acabe9520307821afc835b76af33af
Raghav Gangwa <raghavgangwar222@gmail.com>
2026-03-23 16:16:06 +0530

    initial commit

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter , Link, Route, Routes } from 'react-router-dom';
+import {logo} from './assets';
+import{Home, CreatPost} from './pages';
+const App = () => {
+  return (
+    <BrowserRouter>
+    <header>
+      
+    </header>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/vite-project/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from query('./Home')
+import CreatePost from query('./CreatePost')
+
+export {
+    Home,
+    CreatePost
+}
